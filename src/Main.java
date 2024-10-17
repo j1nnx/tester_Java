012040e27e4b2afb0821a7f48fdad2be48783865
j1nnx <neykerez@gmail.com>
2024-10-17 15:23:01 +0300

    method

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,52 @@
+import java.time.LocalDate;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Задание 1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        System.out.println("Ф. И. О. сотрудника " + lastName + " " + firstName + " " + middleName);
-        System.out.println(" ");
-        System.out.println("Задание 2");
-        String fullName = "Ivanov Ivan Ivanovich";
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
-        System.out.println(" ");
-        System.out.println("Задание 3");
-        String fullName1 = "Иванов Семён Семёнович";
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName1.replace("ё", "е"));
+    public static String checkingYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return year + " год ялвяется високосным";
+        } else
+            return year + " год не является висконсоым";
     }
 
+    public static String appVersion(int number, int YearReal){
+        int currentYear = LocalDate.now().getYear();
+        String yourTelephon;
+        String version;
+        if (number == 0){
+            yourTelephon = "IOS";
+        } else {
+            yourTelephon = "Android";
+        }
+        if (YearReal == currentYear) {
+            version = "обычную версию";
+        } else {
+            version = "облегчённую версию";
+        }
+        return "Установите " + version + "версию приложиения для " + yourTelephon + " по ссылке";
+    }
 
+    public static String deliveryDistanceBank(int deiliveryDistance) {
+        if (deiliveryDistance < 20) {
+            return "Потребуется дней: " + 1;
+        } else if (deiliveryDistance >= 20 && deiliveryDistance < 60) {
+            return "Потребуется дней: " + 2;
+        } else if (deiliveryDistance >= 60 && deiliveryDistance < 100) {
+            return "Потребуется дней: " + 3;
+        } else {
+            return "Извините мы не можем вам доставить";
+        }
+    }
 
+    public static void main(String[] args) {
+        int year = 2021;
+        System.out.println(checkingYear(year));
+        int yourTelephon = 0;
+        int YearReal = 2014;
+        System.out.println(appVersion(yourTelephon, YearReal));
+        int deiliveryDistance = 52;
+        System.out.println(deliveryDistanceBank(deiliveryDistance));
+        }
 }
+
 
 
 
